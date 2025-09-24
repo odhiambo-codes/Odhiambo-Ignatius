85680463ceb4b67ac254dd238b4a5708d2c65deb
odhiambo-codes <odhiamboignatius72@gmail.com>
2025-09-24 12:27:45 +0300

    Add files via upload

---
+++ Program to identify eligibility for loan.cpp
@@ -0,0 +1,28 @@
+/*
+Name:Ignatius Odhiambo wandera 
+Reg No:PA106/G/28770/25
+Description Program to identify eligibility for loan
+*/
+
+#include <stdio.h>
+
+int main() {
+    int age;
+    float income;
+
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Enter your annual income (in Sh): ");
+    scanf("%f", &income);
+
+
+    if (age >= 21 && income >= 21000) {
+        printf("Congratulations you qualify for a loan.\n");
+    } else {
+        printf("Unfortunately, we are unable to offer you a loan at this time.\n");
+    }
+
+    return 0;
+}
