d323e960caf2a9abae9268c1827401df41e3a993
odhiambo-codes <odhiamboignatius72@gmail.com>
2025-10-08 14:13:39 +0300

    Add files via upload

---
+++ listing_of_numbers.c
@@ -0,0 +1,17 @@
+/*
+Name: Ignatius Odhiambo Wandera
+Reg :PA106/G/28770/25
+Description: listing_of_numbers
+*/
+#include<stdio.h>
+//main function
+int main(){
+	
+	//variable declaration
+   	int a;
+   	
+	for(a=100; a>50; a--){	
+	printf("%d\n" ,a);
+	}
+	return 0;
+}
